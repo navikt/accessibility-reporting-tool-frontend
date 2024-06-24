e6e9ec60877b115f959358b992de73fae31b5745
Mats Nylander <mats.thoresen.nylander@nav.no>
2024-06-24 15:13:33 +0200

    set up project files
    Co-authored-by: Jaspreet Kaur <Jaspreet.Kaur@nav.no>

--- mock/server.ts
+++ mock/server.ts
@@ -9,42 +9,8 @@ api.use("/*", cors({
   credentials: true,
 }));
 
-api.get('/utkast/v2/utkast', (c) => {
-  return c.json([
-    {
-      utkastId: "12467899999",
-      tittel: "Søknad om dagpenger, permittert",
-      link: "https://www.intern.dev.nav.no/minside/",
-      opprettet: "2020-03-23T08:53:24.636Z",
-      sistEndret: null,
-      metrics: {
-        skjemakode: "2233/8",
-        skjemanavn: "Dagpenger søknadskjema",
-      },
-    },
-    {
-      utkastId: "124677777777",
-      tittel: "Søknad om foreldre- og svangerskapspenger",
-      link: "https://www.intern.dev.nav.no/minside/",
-      opprettet: "2020-03-13T08:53:24.636Z",
-      sistEndret: "2020-03-14T08:53:24.636Z",
-      metrics: null,
-    },
-    {
-      utkastId: "124679",
-      tittel: "Søknad om dagpenger, permittert",
-      link: "https://www.intern.dev.nav.no/minside/",
-      opprettet: "2020-10-13T08:53:24.636Z",
-      sistEndret: null,
-    },
-    {
-      utkastId: "124610",
-      tittel: "Søknad om foreldre- og svangerskapspenger",
-      link: "https://www.intern.dev.nav.no/minside/",
-      opprettet: "2023-03-13T08:53:24.636Z",
-      sistEndret: "2023-03-14T08:53:24.636Z",
-    },
-  ]);
+api.get('/api', (c) => {
+  return c.json([]);
 });
 
 serve(api);
--- src/middleware/utils.ts
+++
@@ -1,3 +0,0 @@
-import type { APIContext } from "astro";
-
-export const isInternal = (context: APIContext) => context.request.url.includes("/internal");
