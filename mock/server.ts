6e2f114d2a6954ce38602f2e55793177d2ef52ae
Mats Nylander <mats.thoresen.nylander@nav.no>
2024-06-26 15:14:10 +0200

    storing changes

--- mock/server.ts
+++ mock/server.ts
@@ -13,4 +13,21 @@ api.get('/api', (c) => {
   return c.json([]);
 });
 
+api.get('/teams', (c) => {
+  return c.json([
+    {
+      navn: "Team Nav.no",
+      url: "https://www.nav.no/test-rapport"
+    },
+    {
+      navn: "Min side",
+      url: "https://www.nav.no/test-rapport"
+    },
+    {
+      navn: "Team test",
+      url: "https://www.nav.no/test-rapport"
+    },
+  ]);
+});
+
 serve(api);
