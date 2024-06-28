0f694cde92d58b30cae308a77ae01104763641fc
Mats Nylander <mats.thoresen.nylander@nav.no>
2024-06-28 12:23:13 +0200

    Fixed build error

---
+++ src/components/buttons/AddErklaeringBtn.tsx
@@ -0,0 +1,10 @@
+import {Button} from "@navikt/ds-react";
+
+function AddOrgBtn(){
+    return(
+
+    <Button variant="secondary">
+        Lag ny erklæring </Button>
+)}
+
+export default AddOrgBtn;
