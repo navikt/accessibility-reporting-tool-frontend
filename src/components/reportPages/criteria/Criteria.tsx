3c5d8f19971c9c54caba26e011d76e481790b211
Nima Hakimi <nima.hakimi2@nav.no>
2024-07-03 16:17:22 +0200

    Refactor create report page content. Add mock data for criteria in server.ts

---
+++ src/components/reportPages/criteria/Criteria.tsx
@@ -0,0 +1,16 @@
+import Criterion from '.././criterion/Criterion';
+import type { CriteriaProps } from '@src/types';
+
+const Criteria = ({ criteria }: CriteriaProps) => (
+  <div>
+    {criteria.map((criterion) => (
+      <Criterion
+        key={criterion.WCAGId}
+        title={criterion.title}
+        description={criterion.description}
+      />
+    ))}
+  </div>
+);
+
+export default Criteria;
---
+++ src/components/reportPages/criterion/Criterion.tsx
@@ -0,0 +1,38 @@
+import { Textarea, Radio, RadioGroup } from '@navikt/ds-react';
+import { useState } from 'react';
+import type { CriterionProps } from '@src/types';
+import styles from './Criterion.module.css';
+
+const Criterion = (criterion: CriterionProps) => {
+  const [currentState, setCurrentState] = useState<string>('');
+  const handleChange = (val: string) => {
+    setCurrentState(val);
+    console.log(val);
+  };
+
+  return (
+    <div className={styles.criterionWrapper}>
+      <div className={styles.criterion}>
+        <RadioGroup
+          className={styles.radioGroup}
+          legend={criterion.title}
+          onChange={handleChange}
+          description={criterion.description}
+        >
+          <Radio value="yes">Ja</Radio>
+          <Radio value="no">Nei</Radio>
+          <Radio value="notApplicable">Ikke aktuelt</Radio>
+        </RadioGroup>
+        {currentState === 'no' ? (
+          <Textarea
+            className={styles.textarea}
+            label="Hva er grunnen til at du mener kriteriet ikke er oppfylt?"
+            description="Her skal du beskrive promblemene dette kriteriet medfører, og inforfomere brukeren om eventuelle workarounds."
+          />
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default Criterion;
