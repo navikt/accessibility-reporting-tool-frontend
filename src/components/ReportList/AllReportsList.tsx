2e4a772d8df241407fad53440cced7548b81357f
Mats Nylander <mats.thoresen.nylander@nav.no>
2024-07-12 15:33:18 +0200

    Prettier formatting

--- src/components/ReportList/AllReportsList.tsx
+++ src/components/ReportList/AllReportsList.tsx
@@ -1,20 +1,18 @@
-import useSWRImmutable from "swr/immutable";
-import ReportList from "./ReportList";
-import { apiUrl } from "@src/urls";
-import { fetcher } from "@src/utils/api.client";
+import useSWRImmutable from 'swr/immutable';
+import ReportList from './ReportList';
+import { apiUrl } from '@src/urls';
+import { fetcher } from '@src/utils/api.client';
 
-function AllReportsList(){
-    const { data: reportListData, isLoading: isLoadingList } = useSWRImmutable(
-        { url: `${apiUrl}/reports/list` },
-        fetcher,
-      );
+function AllReportsList() {
+  const { data: reportListData, isLoading: isLoadingList } = useSWRImmutable(
+    { url: `${apiUrl}/reports/list` },
+    fetcher,
+  );
 
-    if(isLoadingList){
-      return null;
-    }
+  if (isLoadingList) {
+    return null;
+  }
 
-    return(
-        <ReportList reports={reportListData}/>
-    )
+  return <ReportList reports={reportListData} />;
 }
 export default AllReportsList;
