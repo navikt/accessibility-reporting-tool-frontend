085743f2ceafc33dbc2fce73f987f4c3b1e6fca5
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2024-07-22 13:56:46 +0200

    debug

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -22,8 +22,8 @@ export const onRequest = defineMiddleware(async (context, next) => {
     return context.redirect(loginUrl(context.url.toString()));
   }
 
-  const obo = await requestOboToken(token, `${process.env.NAIS_CLUSTER_NAME}:a11y-statement:a11y-statement`);
-  console.log(`${process.env.NAIS_CLUSTER_NAME}:a11y-statement:a11y-statementasda`)
+  const obo = await requestOboToken(token, `${process.env.NAIS_CLUSTER_NAME}:a11y-statement:a11y-statement-aw`);
+  console.log(`${process.env.NAIS_CLUSTER_NAME}:a11y-statement:a11y-statement`)
   if(!obo.ok){
     console.log("Fail on-behalf-of token for api")
     console.log(obo.error)
