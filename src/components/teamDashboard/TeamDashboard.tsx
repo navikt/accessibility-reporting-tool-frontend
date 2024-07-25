4cbf9c89db053610ce4c3a6af3579d1cc1666d81
Mats Nylander <mats.thoresen.nylander@nav.no>
2024-07-25 13:15:36 +0200

    Prettier

--- src/components/teamDashboard/TeamDashboard.tsx
+++ src/components/teamDashboard/TeamDashboard.tsx
@@ -174,7 +174,7 @@ function TeamDashboard(props: TeamDashboardProps) {
         </Heading>
 
         <ReportList reports={reportListData} />
-          {/*
+        {/*
         <Heading level="2" size="large" spacing>
           Samlerapporter
         </Heading>
