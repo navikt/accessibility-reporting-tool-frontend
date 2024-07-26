cf960bc0739902c9a4bf93e7bdf2ab723c768ec2
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2024-07-26 14:51:48 +0200

    Fix

--- src/utils/serverUtils/fetchFromApi.ts
+++ src/utils/serverUtils/fetchFromApi.ts
@@ -1,39 +1,46 @@
 import { apiUrl, isLocal } from './urls.ts';
 import { getOboToken } from '@src/utils/serverUtils/getOboToken.ts';
 import type { APIContext } from 'astro';
+import { req } from 'astro/dist/core/messages';
 
 export const fetchFromApi = async (context: APIContext, apiURL: URL) => {
-  const oboToken = isLocal ? 'fake token' : await getOboToken(context.request);
-  const method = context.request.method;
-  const requestBody = context.request.body;
-  const requestHeaders = context.request.headers;
-  requestHeaders.append('Authorization', `Bearer ${oboToken}`);
+    const oboToken = isLocal ? 'fake token' : await getOboToken(context.request);
+    const method = context.request.method;
+    const requestBody = context.request.body;
+    const requestHeaders = context.request.headers;
+    requestHeaders.append('Authorization', `Bearer ${oboToken}`);
 
-  const response = await fetch(apiURL.href, {
-    method: method,
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      Authorization: `Bearer ${oboToken}`,
-    },
-    body: JSON.stringify(requestBody),
-  });
 
-  const contentType = response.headers.get('content-type');
+    const requestInit: RequestInit = {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: `Bearer ${oboToken}`
+      }
+    };
 
-  if (!response.ok) {
-    console.log(
-      `Failed to fetch data from api ${apiUrl} status code ${response.status}`,
-    );
-    return new Response(JSON.stringify({}), {
-      status: response.status,
-      headers: response.headers,
-    });
+    if (method != '`GET' && requestBody) {
+      requestInit.body = requestBody;
+    }
+
+    const response = await fetch(apiURL.href, requestInit);
+    const contentType = response.headers.get('content-type');
+
+    if (!response.ok) {
+      console.log(
+        `Failed to fetch data from api ${apiUrl} status code ${response.status}`
+      );
+      return new Response(JSON.stringify({}), {
+        status: response.status,
+        headers: response.headers
+      });
+    }
+    if (contentType && contentType.includes('text')) {
+      return new Response(JSON.stringify(await response.text()));
+    }
+
+    return new Response(JSON.stringify(await response.json()));
   }
-
-  if (contentType && contentType.includes('text')) {
-    return new Response(JSON.stringify(await response.text()));
-  }
-
-  return new Response(JSON.stringify(await response.json()));
-};
+;
+;
