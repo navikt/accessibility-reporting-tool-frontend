890c69e684fdc68377869ef03c4264e0fee28587
Mats Nylander <mats.thoresen.nylander@nav.no>
2024-07-29 16:16:00 +0200

    useSWR instead of useSWRImmutable

--- src/components/ReportList/AllReportsList.tsx
+++ src/components/ReportList/AllReportsList.tsx
@@ -2,9 +2,10 @@ import useSWRImmutable from 'swr/immutable';
 import ReportList from './ReportList';
 import { apiProxyUrl } from '@src/utils/clientUtils/urls.ts';
 import { fetcher } from '@src/utils/clientUtils/api.ts';
+import useSWR from 'swr';
 
 function AllReportsList() {
-  const { data: reportListData, isLoading: isLoadingList } = useSWRImmutable(
+  const { data: reportListData, isLoading: isLoadingList } = useSWR(
     { url: `${apiProxyUrl}/reports/list` },
     fetcher,
   );
