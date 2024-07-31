2810e82adbb0d4c39d37fb761f5e3656a368b844
Sergio A. Arevalo Soria <sergio.alberto.arevalo.soria@nav.no>
2024-07-31 18:34:46 +0200

    Experiment with server-islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,8 @@ export default defineConfig({
   output: 'server',
   adapter: node({
     mode: 'standalone',
-  })
+  }),
+  experimental: {
+    serverIslands: true
+  },
 });
--- src/utils/server/getOboToken.ts
+++ src/utils/server/getOboToken.ts
@@ -1,9 +1,14 @@
 import { requestOboToken } from '@navikt/oasis';
+import { isLocal } from '@src/utils/server/environment.ts';
 
 const API_SCOPE = `api://${process.env.NAIS_CLUSTER_NAME}.a11y-statement.a11y-statement/.default`;
 
 export const getOboToken = async (token: string): Promise<string> => {
   const oboResult = await requestOboToken(token, API_SCOPE);
+
+  if (isLocal) {
+    return 'fake token';
+  }
 
   if (!oboResult.ok) {
     console.log('Fail on-behalf-of token for api');
