e3dd0fea5c5481591f49804963b2997297071d0c
Nima Hakimi <nima.hakimi2@nav.no>
2024-08-22 14:42:25 +0200

    Fix bug in homepage

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -11,54 +11,54 @@ const Home = () => {
     { url: `${apiProxyUrl}/users/details` },
     fetcher,
   );
-
-  return userData?.teams ? (
-    <MyTeam />
-  ) : (
-    <main>
-      <section className={styles.section1}>
-        <section className={styles.innerSection1}>
-          <Heading level="1" size="xlarge">
-            Velkommen til A11y Statement!
-          </Heading>
-          <p className={styles.text}>
-            Denne applikasjonen er NAVs interne rapporteringsverktøy for
-            tilgjengelighetserklæringer
-          </p>
-          <Button
-            as={Link}
-            href="/teams"
-            underline={false}
-            variant="secondary"
-            iconPosition="right"
-            icon={<ArrowRightIcon />}
-          >
-            Finn eller lag ditt team
-          </Button>
+  return (
+    <>
+      {userData?.teams !== undefined && <MyTeam />}
+      <main>
+        <section className={styles.section1}>
+          <section className={styles.innerSection1}>
+            <Heading level="1" size="xlarge">
+              Velkommen til A11y Statement!
+            </Heading>
+            <p className={styles.text}>
+              Denne applikasjonen er NAVs interne rapporteringsverktøy for
+              tilgjengelighetserklæringer
+            </p>
+            <Button
+              as={Link}
+              href="/teams"
+              underline={false}
+              variant="secondary"
+              iconPosition="right"
+              icon={<ArrowRightIcon />}
+            >
+              Finn eller lag ditt team
+            </Button>
+          </section>
         </section>
-      </section>
-      <section className={styles.section2}>
-        <article className={styles.article}>
-          <ComponentIcon title="code icon" fontSize="3rem" />
-          <Heading level="3" size="medium">
-            Les mer på{' '}
-            <Link href="https://aksel.nav.no/god-praksis/universell-utforming">
-              Aksel.no
-            </Link>
-          </Heading>
-        </article>
+        <section className={styles.section2}>
+          <article className={styles.article}>
+            <ComponentIcon title="code icon" fontSize="3rem" />
+            <Heading level="3" size="medium">
+              Les mer på{' '}
+              <Link href="https://aksel.nav.no/god-praksis/universell-utforming">
+                Aksel.no
+              </Link>
+            </Heading>
+          </article>
 
-        <article className={styles.article}>
-          <FigureIcon title="accessibility figure icon" fontSize="3rem" />
-          <Heading level="3" size="medium">
-            Les mer på{' '}
-            <Link href="https://www.uutilsynet.no/veiledning/tilgjengelighetserklaering/1127">
-              uutilsynet.no
-            </Link>
-          </Heading>
-        </article>
-      </section>
-    </main>
+          <article className={styles.article}>
+            <FigureIcon title="accessibility figure icon" fontSize="3rem" />
+            <Heading level="3" size="medium">
+              Les mer på{' '}
+              <Link href="https://www.uutilsynet.no/veiledning/tilgjengelighetserklaering/1127">
+                uutilsynet.no
+              </Link>
+            </Heading>
+          </article>
+        </section>
+      </main>
+    </>
   );
 };
 
