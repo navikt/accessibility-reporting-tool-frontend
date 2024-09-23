61048dbec9de803c0b94a26f75473285f69324dc
Nima Hakimi <nima.hakimi2@nav.no>
2024-09-23 12:59:18 +0200

    Checkbox

--- src/components/reportPages/CreateReport.tsx
+++ src/components/reportPages/CreateReport.tsx
@@ -90,7 +90,7 @@ const CreateReport = ({ report, reportType, isAdmin }: CreateReportProps) => {
 
   const handleCheckboxChange = () => {
     setIsPartOfNavNo(!isPartOfNavNo);
-    updateReportData({ isPartOfNavNo: !isPartOfNavNo });
+    updateReportData({ isPartOfNavNo: isPartOfNavNo });
   };
 
   useEffect(() => {
